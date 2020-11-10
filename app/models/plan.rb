41cd27e8a8c155e83c20218cf972075b0342e37c
k-suke555 <keisuke.k.1991@gmail.com>
2020-11-11 00:11:54 +0900

    計画検索機能の実装

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -18,4 +18,19 @@ class Plan < ApplicationRecord
     validates :image
   end
   validates :area_id, :lunch_type_id, :lunch_price_id, :afternoon_type_id, :afternoon_price_id, :afternoon_period_id, numericality: { other_than: 1, message: 'は--以外を選択してください' }
+
+  def self.search(search)
+    if search != ""
+      Plan.where('title LIKE(?)', "%#{search}%")
+        .or(Plan.where('lunch_title LIKE(?)', "%#{search}%"))
+          .or(Plan.where('lunch_description LIKE(?)', "%#{search}%"))
+            .or(Plan.where('afternoon_title LIKE(?)', "%#{search}%"))
+              .or(Plan.where('afternoon_description LIKE(?)', "%#{search}%"))
+    else
+      Plan.all
+    end
+  end
+
 end
+
+
