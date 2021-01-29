bcd981f907f1aa232e77b36013acda02c9c8a6a1
k-suke555 <keisuke.k.1991@gmail.com>
2021-01-29 19:03:20 +0900

    お気に入り数の表示等変更

--- app/models/lunch_price.rb
+++ app/models/lunch_price.rb
@@ -1,13 +1,13 @@
 class LunchPrice < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '無料~1,000' },
-    { id: 3, name: '~2,000' },
-    { id: 4, name: '~3,000' },
-    { id: 5, name: '~5,000' },
-    { id: 6, name: '~1万' },
-    { id: 7, name: '~2万' },
-    { id: 8, name: '2万~' },
+    { id: 2, name: '無料~1,000円' },
+    { id: 3, name: '~2,000円' },
+    { id: 4, name: '~3,000円' },
+    { id: 5, name: '~5,000円' },
+    { id: 6, name: '~1万円' },
+    { id: 7, name: '~2万円' },
+    { id: 8, name: '2万円~' },
     { id: 9, name: 'その他・不明' }
   ]
 
